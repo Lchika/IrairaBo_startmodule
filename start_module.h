025b86304590c3a603b9e677926ae3276e41f249
nasu1234 <47448269+nasu1234@users.noreply.github.com>
2019-04-14 17:41:43 +0900

    Update start_module.h
    ピンを実際のものと合わせた。また、サーボとLED用のピンを追加。Commitのテスト。

--- start_module.h
+++ start_module.h
@@ -1,18 +1,20 @@
-/* start_define.h            */
+﻿/* start_define.h            */
 /* スタートモジュール関係のヘッダ */
 
 #ifndef _START_MODULE_H_
 #define _START_MODULE_H_
 
 /* Arduino のピン定義 */
-#define PIN_GOAL_START      12  //ゴール判定ピン(スタートモジュール)
-#define PIN_HIT_START       3   //当たった判定ピン(スタートモジュール)
-#define PIN_RX              10  //ソフトウェアシリアルRX
-#define PIN_TX              11  //ソフトウェアシリアルTX
+#define PIN_GOAL_START      2  //ゴール判定ピン(スタートモジュール)
+#define PIN_HIT_START       4   //当たった判定ピン(スタートモジュール)
+#define PIN_RX              11  //ソフトウェアシリアルRX
+#define PIN_TX              13  //ソフトウェアシリアルTX
 #define PIN_DIP_0           6   //DIPスイッチbit0
 #define PIN_DIP_1           7   //DIPスイッチbit1
 #define PIN_DIP_2           8   //DIPスイッチbit2
 #define PIN_DIP_3           9   //DIPスイッチbit3
+#define PIN_SERVO           3   //多回転サーボの信号線用ピン
+#define PIN_LED             5   //スタート部分のLEDの出力用ピン
 
 /* PC とのソフトウェアシリアル通信関係 */
 #define PC_SSERIAL_BAUDRATE     9600  //ボーレート
